9f8800c867b5ff468af6dbbb92143b139e1e9ef3
Maria <maria.temeniati@edu.esiee.fr>
2021-10-22 14:30:40 +0200

    coucou

--- app/src/main/java/com/example/projet_myparis/MainActivity.java
+++ app/src/main/java/com/example/projet_myparis/MainActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 //test
 public class MainActivity extends AppCompatActivity {
-
+ // coucou
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
