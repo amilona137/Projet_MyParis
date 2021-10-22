6209d6a4a6f51e69029d0faa1f768a2c2509cdf2
Maria <maria.temeniati@edu.esiee.fr>
2021-10-22 14:25:39 +0200

    coucou

--- app/src/main/java/com/example/projet_myparis/MainActivity.java
+++ app/src/main/java/com/example/projet_myparis/MainActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    // coucou the Rock
+    // coucou the Rockmm
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
