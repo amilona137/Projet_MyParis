ace842fcb9a28336a4651bae96abf3fa36dce9e0
Maria <maria.temeniati@edu.esiee.fr>
2021-10-26 13:46:20 +0200

    // code pour coder l'appui sur le bouton

--- app/src/main/java/com/example/projet_myparis/MainActivity.java
+++ app/src/main/java/com/example/projet_myparis/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.projet_myparis;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 //test
 public class MainActivity extends AppCompatActivity {
- // coucou
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button boutton = (Button) findViewById(R.id.button);
+        boutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(packageContext))
+            }
+        });
     }
 }
