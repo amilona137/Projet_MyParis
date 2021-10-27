d4288f778ea147468b8a5d7df1ec77524a8f8057
Maria <maria.temeniati@edu.esiee.fr>
2021-10-27 16:09:53 +0200

    // début deuxième page

--- app/src/main/java/com/example/projet_myparis/FirstPage.java
+++ app/src/main/java/com/example/projet_myparis/FirstPage.java
@@ -1,4 +1,14 @@
 package com.example.projet_myparis;
 
-public class FirstPage {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FirstPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first_page);
+    }
 }
