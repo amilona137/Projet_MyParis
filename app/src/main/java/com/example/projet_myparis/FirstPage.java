3773eb2cbf462bb5d72b9dbc697bc4747c6c0954
Amélie <amelie.vautrin@edu.esiee.fr>
2021-11-09 13:28:12 +0100

    Fin de la première page Main activity avec image de fond et partie redirection vers une autre page en cliquant sur la page

--- app/src/main/java/com/example/projet_myparis/FirstPage.java
+++ app/src/main/java/com/example/projet_myparis/FirstPage.java
@@ -1,5 +1,5 @@
 package com.example.projet_myparis;
-
+import android.content.Intent;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
--- app/src/main/java/com/example/projet_myparis/MainActivity.java
+++ app/src/main/java/com/example/projet_myparis/MainActivity.java
@@ -2,10 +2,16 @@ package com.example.projet_myparis;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.text.Layout;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 //test
 public class MainActivity extends AppCompatActivity {
@@ -13,14 +19,25 @@ public class MainActivity extends AppCompatActivity {
     private final float TOUCH_SCALE_FACTOR = 180.0f / 320;
     private float previousX;
     private float previousY;
-   // @Override
+    RelativeLayout Layout;
+
+    // @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        Layout = findViewById(R.id.textView5);
+        Layout.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionevent) {
+                Intent intent = new Intent(
+                        MainActivity.this,
+                        FirstPage.class
+                );
+                startActivity(intent);
+                return false;
+            }
 
-    public boolean onTouch(View v, MotionEvent event) {
-        
-        return true;
+
+        });
     }
 }
