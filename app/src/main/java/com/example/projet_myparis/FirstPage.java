fcf8cf65d6fc92b3afa6bc24550a2ab7394afbf9
Maria <maria.temeniati@edu.esiee.fr>
2021-11-10 17:58:26 +0100

    // continuation et fin de la page de menu firstPage

--- app/src/main/java/com/example/projet_myparis/FirstPage.java
+++ app/src/main/java/com/example/projet_myparis/FirstPage.java
@@ -1,14 +1,75 @@
 package com.example.projet_myparis;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class FirstPage extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first_page);
+
+        /*findViewById(R.id.bmonuments).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(FirstPage.this, MonumentsPage.class));
+            }
+        });
+
+        findViewById(R.id.brestaurants).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(FirstPage.this, RestaurantsPage.class));
+            }
+        });
+        findViewById(R.id.bdormir).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(FirstPage.this, DormirPage.class));
+            }
+        });
+        findViewById(R.id.bshopping).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(FirstPage.this, ShoppingPage.class));
+            }
+        });
+        findViewById(R.id.bevenements).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(FirstPage.this, EvenementsPage.class));
+            }
+        });
+        findViewById(R.id.bplans).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(FirstPage.this, BonsPlansPage.class));
+            }
+        });
+        findViewById(R.id.bcircuit).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(FirstPage.this, CircuitPage.class));
+            }
+        });
+        findViewById(R.id.banecdotes).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(FirstPage.this, AnecdotesPage.class));
+            }
+        });
+        findViewById(R.id.btransport).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(FirstPage.this, TransportPage.class));
+            }
+        });*/
+
     }
-}
+    }
